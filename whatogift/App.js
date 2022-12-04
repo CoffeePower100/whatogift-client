ee866704df96e1fb954ee7bd44584727ba3ed1b0
CoffeePower100 <64426956+CoffeePower100@users.noreply.github.com>
2022-12-04 15:53:36 +0200

    Added swagger docs to account requests

--- whatogift/App.js
+++ whatogift/App.js
@@ -1,17 +1,46 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { TabsNavigator, AccountStack } from './src/navigation';
 
+import * as Location from 'expo-location';
+
 export default function App() {
 
   const [isLogin, setIsLogin] = useState(!false);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   return (
-    <NavigationContainer>
+    <View>
+      <Text>{text}</Text>
+    </View>
+    /*<NavigationContainer>
       {
         isLogin ? (<TabsNavigator />) : (<AccountStack />)
       }
     </NavigationContainer>
+    */
   );
 }
